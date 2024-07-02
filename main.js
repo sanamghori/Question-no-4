c73aeae60355fd040a0660fddab2faabbc1e9feb
sanamghori <161497237+sanamghori@users.noreply.github.com>
2024-07-02 21:33:33 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+//question # 4
+/**Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+ * Your output should look something like the following, including the quotation marks:
+ * Albert Einstein once said, “A person who never made a mistake never tried anything new.” */
+let FamousQuote = "The reward of deeds depends upon the intentions";
+let Author = "Hazrat muhmmad pbuh";
+// Print the quote and the name of its author.
+console.log(`${Author} once said; "${FamousQuote}"`);
